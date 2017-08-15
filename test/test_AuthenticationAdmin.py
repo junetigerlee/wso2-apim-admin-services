c08c54662d2a802b46f88e35626d7a2ec15474f8
junetigerlee <junetigerlee@gmail.com>
2017-08-15 09:14:41 +0800

    first commit

--- test/test_AuthenticationAdmin.py
+++ test/test_AuthenticationAdmin.py
@@ -5,13 +5,18 @@ from zeep.transports import Transport
 session = Session()
 session.verify = False
 
-# client = Client('https://10.16.118.52:9443/services/AuthenticationAdmin?wsdl',
-#                 transport=Transport(session=session))
-
-client = Client('../wsdl/AuthenticationAdmin.xml',
+client = Client('https://10.16.118.52:9443/services/AuthenticationAdmin?wsdl',
                 transport=Transport(session=session))
 
-print client.service.login('admin', 'admin', 'localhost')
+# client = Client('../wsdl/AuthenticationAdmin.xml',
+#                 transport=Transport(session=session))
 
-print client.service.login('zhanlif', '123456', 'localhost')
+service = client.create_service('{http://authentication.services.core.carbon.wso2.org}AuthenticationAdminSoap11Binding',
+                                'https://10.16.118.52:9443/services/AuthenticationAdmin')
+
+print service.login('admin', 'admin', 'localhost')
+
+# print client.service.login('admin', 'admin', 'localhost')
+
+# print client.service.login('zhanlif', '123456', 'localhost')
 
